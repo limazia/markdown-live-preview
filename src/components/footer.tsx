62c62431450e187ce61852b93c1a8e1b99449b24
Acacio de Lima <limadeacacio@gmail.com>
2024-08-13 22:13:14 -0300

    chore: update footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,18 +1,16 @@
 export function Footer() {
   return (
-    <footer className="w-full h-10 flex items-center justify-center text-xs text-white bg-[#444] py-2 px-4 z-10">
-      <div>
-        <span>
-          This is a Next.js version of the website{" "}
-          <a
-            href="https://markdownlivepreview.com/"
-            target="_blank"
-            className="font-semibold text-gray-300"
-          >
-            markdownlivepreview.com
-          </a>
-        </span>
-      </div>
+    <footer className="w-full h-10 flex items-center justify-center text-xs text-center text-white bg-[#444] py-2 px-4 z-10">
+      <span>
+        This is a Next.js version of the website{" "}
+        <a
+          href="https://markdownlivepreview.com/"
+          target="_blank"
+          className="font-semibold text-gray-300"
+        >
+          markdownlivepreview.com
+        </a>
+      </span>
     </footer>
   );
 }
